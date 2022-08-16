6d3a324044f800e7e4b46b337fe28981ebb49a8e
Laura Latuillerie <laura.latuillerie@gmail.com>
2022-08-16 16:22:40 +0200

    Submit exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+Article.destroy_all
+
+10.times do
+  Article.create(
+    title: "#{Faker::JapaneseMedia::OnePiece.character} | #{Faker::JapaneseMedia::OnePiece.akuma_no_mi}",
+    content: Faker::Lorem.paragraph(sentence_count: 40)
+    )
+end
